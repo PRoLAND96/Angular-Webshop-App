be23a2bdcf50135b9af1b7616ee35494e2103da2
PRoLAND96 <petoroli2001@gmail.com>
2024-11-21 18:17:09 +0100

    create a webshop

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-Webshop-App' title`, () => {
+  it(`should have the 'Angural Webshop App' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-Webshop-App');
+    expect(app.title).toEqual('Angural Webshop App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-Webshop-App');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tutorial');
   });
 });
